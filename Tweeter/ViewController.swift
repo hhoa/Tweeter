dc32461485652800fc140f495a08050ff4180e17
Why having name <hoangkhahoa@gmail.com>
2019-04-07 09:07:19 +0700

    setup new project

---
+++ Tweeter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tweeter
+//
+//  Created by Hoa Hoang on 4/7/19.
+//  Copyright © 2019 Hoa Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
